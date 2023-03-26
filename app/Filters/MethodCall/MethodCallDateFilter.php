8fd5cc7fee4e65bd3adde3b0559d0863e396e0a9
grkun <gr.kun.ov@gmail.com>
2023-03-27 01:35:48 +0700

    -- add date_from filter and date_end -- fix between filter

--- app/Filters/MethodCall/MethodCallDateFilter.php
+++ app/Filters/MethodCall/MethodCallDateFilter.php
@@ -15,7 +15,11 @@ class MethodCallDateFilter implements Pipe
     public function apply($methodCall, Closure $next)
     {
         if (request()->has('date_from') && request()->has('date_end')) {
-            $methodCall->whereBetween('created_at', [request()->has('date_from'),request()->has('date_end')]);
+            $methodCall->whereBetween('created_at', [request()->get('date_from'),request()->get('date_end')]);
+        }elseif (request()->has('date_from')){
+            $methodCall->where('created_at', '>=',request()->get('date_from'));
+        }elseif(request()->has('date_end')){
+            $methodCall->where('created_at', '<=',request()->get('date_end'));
         }
 
         return $next($methodCall);
